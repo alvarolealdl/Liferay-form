bba5b8e06f6eb4da402b7574b94c42868addd970
brunoquei2105 <bruno.queiroz@liferay.com>
2021-11-29 07:51:46 -0300

    Add web layer to the project AMF

---
+++ modules/amf-web/src/main/java/com/liferay/amf/web/constants/RegistrationPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.amf.web.constants;
+
+/**
+ * @author me
+ */
+public class RegistrationPortletKeys {
+
+	public static final String REGISTRATION =
+		"com_liferay_amf_web_RegistrationPortlet";
+
+}
