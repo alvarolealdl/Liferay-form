a21a0c2ee156c4cb12a37dc4f67c7740efe49659
brunoquei2105 <bruno.queiroz@liferay.com>
2022-01-04 16:12:44 -0300

    Fix import at AcmeLoginMVCActionCommand

--- modules/amf-web/src/main/java/com/liferay/amf/web/portlet/action/AcmeLoginMVCActionCommand.java
+++ modules/amf-web/src/main/java/com/liferay/amf/web/portlet/action/AcmeLoginMVCActionCommand.java
@@ -3,28 +3,32 @@ package com.liferay.amf.web.portlet.action;
 import com.liferay.amf.exception.LoginValidationExceptionException;
 import com.liferay.amf.model.Registration;
 import com.liferay.amf.service.LoginService;
+import com.liferay.amf.web.constants.AcmeMoviePortletKeys;
+import com.liferay.amf.web.constants.MCVCommandNames;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
+import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
 
-//@Component(
-//        immediate = true,
-//        property = {
-//                "javax.portlet.name=" + AcmeMoviePortletKeys.LOGIN,
-//                "mvc.command.name=" + MCVCommandNames.ACCESS_LOGIN,
-//
-//        },
-//        service = MVCActionCommand.class
-//)
+@Component(
+        immediate = true,
+        property = {
+                "javax.portlet.name=" + AcmeMoviePortletKeys.LOGIN,
+                "mvc.command.name=" + MCVCommandNames.ACCESS_LOGIN,
+
+        },
+        service = MVCActionCommand.class
+)
 public class AcmeLoginMVCActionCommand extends BaseMVCActionCommand {
 
     @Override
