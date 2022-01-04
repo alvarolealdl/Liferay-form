144ead1dba56b91ea99bfa653f46d501611011bf
brunoquei2105 <bruno.queiroz@liferay.com>
2022-01-04 16:16:44 -0300

    Fix import problem mvcActionCommand login

--- modules/amf-web/src/main/java/com/liferay/amf/web/portlet/action/AcmeLoginMVCActionCommand.java
+++ modules/amf-web/src/main/java/com/liferay/amf/web/portlet/action/AcmeLoginMVCActionCommand.java
@@ -2,8 +2,13 @@ package com.liferay.amf.web.portlet.action;
 
 import com.liferay.amf.exception.LoginValidationExceptionException;
 import com.liferay.amf.model.Registration;
-import com.liferay.amf.service.*;
+import com.liferay.amf.service.LoginService;
+import com.liferay.amf.service.RegistrationEventService;
+import com.liferay.amf.service.RegistrationLocalServiceUtil;
+import com.liferay.amf.web.constants.AcmeMoviePortletKeys;
+import com.liferay.amf.web.constants.MCVCommandNames;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.servlet.SessionErrors;
@@ -12,6 +17,7 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 import javax.portlet.ActionRequest;
@@ -19,15 +25,15 @@ import javax.portlet.ActionResponse;
 import java.util.Date;
 
 
-//@Component(
-//        immediate = true,
-//        property = {
-//                "javax.portlet.name=" + AcmeMoviePortletKeys.LOGIN,
-//                "mvc.command.name=" + MCVCommandNames.ACCESS_LOGIN,
-//
-//        },
-//        service = MVCActionCommand.class
-//)
+@Component(
+        immediate = true,
+        property = {
+                "javax.portlet.name=" + AcmeMoviePortletKeys.LOGIN,
+                "mvc.command.name=" + MCVCommandNames.ACCESS_LOGIN,
+
+        },
+        service = MVCActionCommand.class
+)
 public class AcmeLoginMVCActionCommand extends BaseMVCActionCommand {
 
     @Override
